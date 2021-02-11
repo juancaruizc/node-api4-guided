35f74aa73b3ca6d3a061daf90962df1c4bcfa502
juancaruizc <juancruizcruz@gmail.com>
2021-02-11 11:11:43 -0700

    updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/*', (_, res) => {
+app.use('/api/mars', (_, res) => {
   res.json({ data: 'Hello from mars' });
 });
 
